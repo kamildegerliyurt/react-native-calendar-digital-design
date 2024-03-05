8b828284cf4062cdcbac73f299257a30efd93781
kamildegerliyurt <degerliyurtkamil@gmail.com>
2024-03-05 17:12:26 +0300

    react-native-calendar-digital-design

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import {Calendar} from 'react-native-calendars';
 
-export default function App() {
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView style={styles.container}>
+
+      <Text style={styles.calendarText}>Calendar</Text>
+
+      <Calendar style={styles.calendar}
+                enableSwipeMonths
+                theme={{monthTextColor:"yellow",
+                        textMonthFontSize:30,
+                        textDayFontWeight:"bold",
+                        arrowColor:"yellow",
+                        calendarBackground:"black",
+                        dayTextColor:"red",
+                        textInactiveColor:"green",
+                        textSectionTitleColor:"yellow",
+                        textDayFontWeight:"bold",
+                        textDisabledColor:"green",}}/>
+
+    </SafeAreaView>
+  )
 }
+
+export default App
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex:1,
+    alignItems:"center",
+    justifyContent:"center",
+    backgroundColor:"black",
+    borderWidth:2,
+    borderColor:"gray",
   },
-});
+  calendarText: {
+    color:"white",
+    fontSize:30,
+    fontWeight:"bold",
+    marginBottom:20,
+
+  },
+  calendar: {
+    backgroundColor:"black",
+    borderBottomWidth:1,
+    borderRightWidth:2,
+    borderLeftWidth:2,
+    borderTopWidth:5,
+    borderColor:"#b6b6b6",
+    borderRadius:20,
+    padding:20,
+     
+  }
+})
---
+++ readme.md
@@ -0,0 +1,18 @@
+*********************************
+STEP 1: npm i react-native-calendars 
+*********************************
+STEP 2: import {Calendar} from 'react-native-calendars';
+*********************************
+STEP 3: <Calendar style={styles.calendar}
+                enableSwipeMonths
+                theme={{monthTextColor:"yellow",
+                        textMonthFontSize:30,
+                        textDayFontWeight:"bold",
+                        arrowColor:"yellow",
+                        calendarBackground:"black",
+                        dayTextColor:"red",
+                        textInactiveColor:"green",
+                        textSectionTitleColor:"yellow",
+                        textDayFontWeight:"bold",
+                        textDisabledColor:"green",}}/>
+*********************************
